53fc0324da5cc8ed10fe704141d78b9e06f60693
Barnamoy Dhar <barnamoy555@gmail.com>
2024-10-25 21:17:59 +0530

    conversion examples given

---
+++ 1_basics/03_conversionOperations.js
@@ -0,0 +1,13 @@
+let score =null
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
