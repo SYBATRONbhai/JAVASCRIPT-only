1ed77982d2292d692a0357cde53d2c7012c74822
Barnamoy Dhar <barnamoy555@gmail.com>
2024-10-26 12:43:44 +0530

    Basics Learned

--- 1_basics/test.js
+++
@@ -1,6 +0,0 @@
- const name = "Barnamoy";
- const age = 23;
- const city = "Raiganj";
- const country = "India";
- const greeting = "Hello, my name is " + name + ". I'm " + age + " years old, and I live in " + city + ", " + country + ".";
- console.log(greeting);
